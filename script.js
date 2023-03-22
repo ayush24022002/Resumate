14bc0de542eab75381e77ec77cfe1603476067a5
Ayush Prakash <84738665+ayush24022002@users.noreply.github.com>
2023-03-22 19:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+// function addNewWEField(){
+//     let newNode=document.createElement('textarea');
+//     newNode.classList.add("form-control");
+//     newNode.id="new";
+//     newNode.classList.add("weField");
+//     newNode.classList.add("mt-2");
+//     newNode.setAttribute("rows",3);
+//     newNode.setAttribute("placeholder","Enter here");
+
+//     let weOb=document.getElementById("we");
+//     let weAddButtonOb=document.getElementById("weAddButton");
+
+//     weOb.insertBefore(newNode,weAddButtonOb);
+// }
+
+let i=0
+let j=0
+const divWrapper = document.getElementById("we");
+const divWrapper2 = document.getElementById("aq");
+
+function addNewWEField(){
+    i=i+1
+    let inputWrapper = document.createElement('div');
+    inputWrapper.id="new-we-${i}";
+    let newNode=document.createElement('textarea');
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows",3);
+    newNode.setAttribute("placeholder","Enter here");
+    inputWrapper.appendChild(newNode)
+    
+    const removeButton = document.createElement('button');
+    removeButton.classList.add("btn")
+    removeButton.classList.add("btn-primary")
+    removeButton.classList.add("btn-sm")
+    removeButton.innerHTML = 'Remove';
+    removeButton.style.marginTop = '8px';
+    removeButton.onclick = () => { 
+        divWrapper.removeChild(inputWrapper)
+    }
+    inputWrapper.appendChild(removeButton);
+    divWrapper.appendChild(inputWrapper);
+  }
+
+// function addNewAQField(){
+//     let newNode=document.createElement("textarea");
+//     newNode.classList.add("form-control");
+//     newNode.classList.add("aqField");
+//     newNode.classList.add("mt-2");
+//     newNode.setAttribute("rows",3);
+//     newNode.setAttribute("placeholder","Enter here");
+
+//     let aqOb=document.getElementById("aq");
+//     let aqAddButtonOb=document.getElementById("aqAddButton");
+
+//     aqOb.insertBefore(newNode,aqAddButtonOb);
+
+// }
+
+function addNewAQField(){
+    i=i+1
+    let inputWrapper = document.createElement('div');
+    inputWrapper.id="new-aq-${i}";
+    let newNode=document.createElement('textarea');
+    newNode.classList.add("form-control");
+    newNode.classList.add("aqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows",3);
+    newNode.setAttribute("placeholder","Enter here");
+    inputWrapper.appendChild(newNode)
+    
+    const removeButton = document.createElement('button');
+    removeButton.classList.add("btn")
+    removeButton.classList.add("btn-primary")
+    removeButton.classList.add("btn-sm")
+    removeButton.innerHTML = 'Remove';
+    removeButton.style.marginTop = '8px';
+    removeButton.onclick = () => { 
+        divWrapper2.removeChild(inputWrapper)
+    }
+    inputWrapper.appendChild(removeButton);
+    divWrapper2.appendChild(inputWrapper);
+  }
+
+//generating cv
+function generateCV(){
+
+    let nameField=document.getElementById("nameField").value;
+    let nameT1=document.getElementById("nameT1");
+    nameT1.innerHTML=nameField;
+
+    // direct
+    document.getElementById("nameT2").innerHTML=nameField;
+
+    // contacts
+    document.getElementById("contactT").innerHTML=document.getElementById("contactField").value;
+     
+    // address
+    document.getElementById("addressT").innerHTML=document.getElementById("addressField").value;
+
+    // email-id
+    document.getElementById("emailT").innerHTML=document.getElementById("emailField").value;
+
+    //linkedIn
+    document.getElementById("linkedinT").innerHTML=document.getElementById("linkedinField").value;
+
+    //Github
+    document.getElementById("githubT").innerHTML=document.getElementById("githubField").value;
+
+    //objective
+    document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
+
+    //workExperience
+    let wes=document.getElementsByClassName("weField");
+    let str="";
+    for(let e of wes){
+        str=str+`<li> ${e.value} </li>`
+    }
+
+    document.getElementById("weT").innerHTML=str;
+
+    //academicQualification
+    let aqs=document.getElementsByClassName("aqField");
+    let str1="";
+    for(let e of aqs){
+        str1=str1+`<li> ${e.value} </li>`
+    }
+
+    document.getElementById("aqT").innerHTML=str1;
+
+    // code for setting image
+    let file=document.getElementById("imgField").files[0];
+    console.log(file);
+    let reader=new FileReader();
+    reader.readAsDataURL(file);
+    console.log(reader.result);
+
+    // set image to template
+    reader.onloadend=function(){
+        document.getElementById("imgTemplate").src=reader.result;
+    };
+
+
+    // display and hide cv
+    document.getElementById("cv-form").style.display="none";
+    document.getElementById("cv-template").style.display="block";
+
+}
+
+
+    //print portfolio
+function printPortfolio(){
+    window.print();
+}
+
+document.getElementById("mailing").write('<a class="btn" href="mailto:ayushzac123@gmail.com">MAIL ME</a>')
